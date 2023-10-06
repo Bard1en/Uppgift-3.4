8e091648a97617f0e64389b411776925f3b520b3
Bard1en <greger.grym@gmail.com>
2023-10-06 12:57:50 +0200

    Add project files.

---
+++ Uppgift 3.4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Uppgift3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång är din låt?");
+            string låten = Console.ReadLine();
+            int ochindex = låten.IndexOf(" ");
+            string minut = låten[..ochindex];
+            string sekund = låten[(ochindex + 1)..];
+            int sekunder = int.Parse(sekund);
+            int minuter = int.Parse(minut);
+            switch (minuter)
+            {
+                case 2:
+
+                    if (sekunder < 45 || sekunder > 60)
+                    {
+                        Console.WriteLine("Din låt får inte spelas");
+                    }
+                    break;
+
+                case 3:
+                    {
+                        if (sekunder > 0 || sekunder > 60)
+                        {
+                            Console.WriteLine("Din låt får spelas");
+                        }
+                        break;
+                    }
+                case 4:
+                    {
+                        if (sekunder < 20 || sekunder > 60) ;
+                        {
+                            Console.WriteLine("Din låt får spelas");
+                        }
+                        break;
+
+
+                    }
+                default:
+                    {
+                        Console.WriteLine("Din låt får inte spelas");
+                        break;
+                    }
+            }
+        }
+    }
+}
